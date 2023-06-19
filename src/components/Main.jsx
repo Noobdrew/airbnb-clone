d12bcd1c452877861d125d7e99393687c37c1080
Ivan Marinski <noobdrew23@gmail.com>
2023-06-19 14:23:12 +0300

    heaver and main done

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import photoGrid from "../assets/photo-grid.png";
+
+export default function Main() {
+  return (
+    <main className="hero">
+      <div className="photo-conteiner">
+        <img src={photoGrid} alt="Hero photo" className="main-photo" />
+      </div>
+
+      <div className="text-conteiner">
+        <h1 className="main-title">Online Experiences</h1>
+        <p className="main-text">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import airbnb from "../assets/airbnb-logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={airbnb} alt="Airbnb logo" width="80px" />
+    </nav>
+  );
+}
