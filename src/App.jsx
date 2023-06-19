f9f860d089ee679efb7fef789d22247eddcfc65a
Ivan Marinski <noobdrew23@gmail.com>
2023-06-19 18:46:10 +0300

    cards use js object to fill in data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Card from "./components/Card";
-
+import data from "./data";
+console.log(data);
 function App() {
+  const cardElement = data.map((e) => {
+    return <Card key={e.id} item={e} />;
+  });
+
   return (
-    //
     <div>
       <Navbar />
       <Main />
-      <Card />
+      <div className="card-conteiner">{cardElement}</div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,27 @@
 import star from "../assets/star.png";
-import katie from "../assets/katie-zaferes.png";
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props);
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "Sold Out";
+  } else if (props.item.location == "Online") {
+    badgeText = "Online";
+  }
   return (
     <div className="card">
-      <img src={katie} alt="Katie zafares" />
-      <div className="img-text">Sold Out</div>
+      <img src={props.item.coverImg} alt="Katie zafares" />
+      {badgeText && <div className="img-text">{badgeText}</div>}
       <div className="description">
         <div className="ratings">
           <img src={star} alt="star" />
-          <div className="rating-text-1">5.0</div>
-          <div className="rating-text-2">(6)</div>
-          <div className="rating-text-3">• USA</div>
+          <div className="rating-text-1">{props.item.stats.rating}</div>
+          <div className="rating-text-2">({props.item.stats.reviewCount})</div>
+          <div className="rating-text-3">• {props.item.location}</div>
         </div>
-        <div className="description-text">Life lessons with Katie Zaferes</div>
+        <div className="description-text">{props.item.title}</div>
         <div className="price">
-          <strong>From $136</strong> / person
+          <strong>From ${props.item.price}</strong> / person
         </div>
       </div>
     </div>
