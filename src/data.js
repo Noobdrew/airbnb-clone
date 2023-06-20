17f3d458b07115627b2dbb715b831d17cea55342
Ivan Marinski <noobdrew23@gmail.com>
2023-06-20 14:43:38 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ export default [
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: katie,
+        coverImg: './components/assets/katie-zaferes.png',
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -22,7 +22,7 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: wedding,
+        coverImg: './components/assets/wedding-photography.png',
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -35,7 +35,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: mountainBike,
+        coverImg: './components/assets/mountain-bike.png',
         stats: {
             rating: 4.8,
             reviewCount: 2
